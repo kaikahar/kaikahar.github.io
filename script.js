d23a5b992052da300b3fd612b2cde3412d5dd2f4
Kahaer Kaiwusaer <kevsarq12@gmail.com>
2022-10-11 00:04:31 -0600

    final

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const navmenu = document.querySelector(".nav-menu");
+var messageArray = ["Nothing to show yet, dank projects coming soon..."];
+var textPosition = 0;
+var speed = 100;
+
 
 hamburger.addEventListener("click", () =>{
     navmenu.classList.toggle("active");
@@ -9,3 +13,12 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navmenu.classList.remove("active");
 }))
+
+typewriter = () =>{
+    document.querySelector(".demo").innerHTML = messageArray[0].substring(0, textPosition) + "<span>\u25ae</span>";
+
+    if(textPosition++ != messageArray[0].length)
+        setTimeout(typewriter, speed);
+}
+
+window.addEventListener("load", typewriter);
