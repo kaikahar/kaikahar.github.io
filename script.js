b65858bed42259358ffde487d7a6c78b2ab34f84
Kahaer Kaiwusaer <kevsarq12@gmail.com>
2022-10-10 21:52:10 -0600

    js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const navmenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", function() {
-    console.log('btn clicked');
-    hamburger.classList.toggle("active");
+hamburger.addEventListener("click", () =>{
     navmenu.classList.toggle("active");
 })
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", function() {
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
     hamburger.classList.remove("active");
     navmenu.classList.remove("active");
 }))
